e757ac6de3eceec304bef8b5a0b3798aaa9fda89
Charles Bevan <798172+cfbevan@users.noreply.github.com>
2009-10-07 16:10:39 +0000

    EOD: Oct7,09 mostly worked on util

--- GISpatialNet/src/us/jonesrychtar/gispatialnet/util.java
+++ GISpatialNet/src/us/jonesrychtar/gispatialnet/util.java
@@ -8,9 +8,13 @@
  */
 package us.jonesrychtar.gispatialnet;
 
+import java.awt.Dimension;
+import javax.naming.OperationNotSupportedException;
 import org.ujmp.core.Matrix;
-import org.ujmp.core.MatrixFactory;
+import org.ujmp.core.calculation.Calculation;
 import org.ujmp.core.objectmatrix.EmptyMatrix;
+import us.jonesrychtar.gispatialnet.Writer.*;
+import us.jonesrychtar.gispatialnet.Reader.*;
 
 /**
  *
@@ -25,22 +29,82 @@ public class util {
      */
     private Matrix x = new EmptyMatrix(); //vector matrix (1 col) of x coordinates
     private Matrix y = new EmptyMatrix(); //vector matrix of y coordinates
-    private Matrix adj = new EmptyMatrix(); //matrix of size x by y where if ij >= 1 there is a line connecting (x,y)i to (x,y)j
-    private Matrix attb = new EmptyMatrix(); //attributes for node (x,y)i where i is the row of attb
+    private Matrix adj = new EmptyMatrix(); //matrix of size x by y where if ij >= 1 there is a line connecting (xi,yi) to (xj,yj)
+    private Matrix attb = new EmptyMatrix(); //attributes for node (xi,yi) where i is the row of attb
 
     private String[] loadedFiles = new String[]{};
 
     //loading functions
 
+       /* FileType:
+     * 0 Shapefile
+     * 1 Shapefile Unknown XY
+     * 2 Google Earth (.kml)
+     * 3 Pajek (.net)
+     * 4 DL/UCINET (.txt, .dat)
+     * 5 Excel (.csv, .xls)
+     * 6 txt file (.txt)
+     *
+     * Matrix:
+     * 0 XYAttb
+     * 1 Adj
+     * 2 X
+     * 3 Y
+     * 4 Attb
+     *
+     * MatrixType
+     * 0 Full Matrix
+     * 1 Lower Matrix
+     * 2 Upper Matrix
+     * ...
+     * */
+        
     //saving functions
-
-    public void SaveAs(Matrix in, String filneame, int Type){
-        switch(Type){
-            case 0: 
+    //TODO: comment functions
+    public void saveShapefile(){
+        if (attb != null) {
+            new convertKnown(x, y, adj, attb);
+        } else {
+            new convertKnown(x, y, adj);
         }
     }
+    public void saveShapefileUnknown(int alg, int Height, int Width){
+        Dimension temp = new Dimension(Height,Width);
+        new convertUnknown(x,y, alg , temp);
+    }
+    public void saveGoogleEarth(String filename){
+        new KMLwriter(combineXYAttb(),filename);
+    }
+    public void savePajek(String filename){
+        new PajekWriter(x.appendHorizontally(y), adj, filename).WriteFile();
+    }
+    public void saveDL(String filename, int ext){
+        new DLwriter(adj,filename,ext).WriteFile();
+    }
+    public void saveExcel(String filenameNodes, String filenameArcs){
+        new ExcelWriter(combineXYAttb(), filenameNodes).WriteFile();
+        new ExcelWriter(adj, filenameArcs).WriteFile();
+    }
+    public void saveCSV(String filenameNodes, String filenameArcs, char seperator){
+        new CSVwriter(combineXYAttb(), filenameNodes, seperator);
+        new CSVwriter(adj, filenameArcs, seperator);
+    }
+   
     //analyzing functions
-
+    public void Border(int alg){
+        Borders b = new Borders(x,y,adj,alg);
+        b.Write();
+    }
+    public void QAP(String arg[]){
+        qap q = new qap (arg.length-2, arg);
+    }
+    public void Highlight(int alg, String filename){
+        HighlightEdges h = new HighlightEdges(x,y,adj,filename,alg);
+        h.write();
+    }
+    public void SNB() throws OperationNotSupportedException{
+        throw new OperationNotSupportedException("function not done yet");
+    }
 
     //extra functions
     public String Status(int Detail){
@@ -114,9 +178,29 @@ public class util {
         }
         return out;
     }
-    public Matrix combineXYAttb(){
-        //TODO: Size Check
-       return x.appendHorizontally(y).appendHorizontally(attb);
-    }
+    public void ClearData(){
+        x = new EmptyMatrix();
+        y = new EmptyMatrix();
+        adj = new EmptyMatrix();
+        attb = new EmptyMatrix(); 
 
+        loadedFiles = new String[]{};
+
+    }
+    public Matrix combineXYAttb() throws IllegalArgumentException{
+       if(x.getRowCount() == y.getRowCount() && y.getRowCount() == attb.getRowCount())
+            return x.appendHorizontally(y).appendHorizontally(attb);
+       else
+           throw new IllegalArgumentException("Matrix Sizes do not match.");
+    }
+    public Matrix[] splitXYAttb(Matrix in){
+        Matrix[] out = new Matrix[3];
+        //copy all rows of col 0 to out[0]
+        out[0] = in.selectColumns(Calculation.Ret.NEW, 0);
+        //copy all rows of col 1 to out[1]
+        out[1] = in.selectColumns(Calculation.Ret.NEW, 1);
+        //copy rest to out[2]
+        out[2] = in.selectColumns(Calculation.Ret.NEW, 2, in.getColumnCount());
+        return out;
+    }
 }
