46692551e0f1860d5dd4ba6205f779b7ef4312d1
Samuel C. Tyler <skamansam@gmail.com>
2009-10-19 02:30:57 +0000

    finished going through Robert's code - most of what we need is in us.jonesrychtar.socialnetwork.SpatialGraph.*, so not much was changed. Probably won't need any of the other classes I created in us.jonesrychtar.socialnetwork.* . Also started (maybe finished?) the PajekReader.

--- GISpatialNet/src/us/jonesrychtar/socialnetwork/CoordinateGraph/XY.java
+++ GISpatialNet/src/us/jonesrychtar/socialnetwork/CoordinateGraph/XY.java
@@ -1,40 +1,46 @@
-
 /*
-* To change this template, choose Tools | Templates
-* and open the template in the editor.
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
  */
 package us.jonesrychtar.socialnetwork.CoordinateGraph;
 
 /**
- *
+ * 
  * @author Samuel C. Tyler <skamansam@gmail.com>
  */
 public class XY extends CoordinateGraphBase {
-    private double xCoord, yCoord;
 
-    // TODO: Here is Sam
-    public void readValue(java.util.Scanner sc) {
-        xCoord = sc.nextDouble();
-        yCoord = sc.nextDouble();
+	@Override
+	public void setCoordinateFormat() {
+		// TODO Auto-generated method stub
+		super.setCoordinateFormat();
+	}
+//TODO: This class is not necessary, as we have readers for all matrices
+/*	private double xCoord, yCoord;
+	Matrix x, y;
 
-        if ((xCoord == 9999) || (yCoord == 9999)) {
-            missingNodes.add(i);
-        } else {
-            x.setAsDouble(xCoord, i, 0);
-            y.setAsDouble(yCoord, i, 0);
-            totalXCoord += xCoord;
-            totalYCoord += yCoord;
-        }
-    }
+	public void readValue(java.util.Scanner sc) {
 
-    public boolean isValidNodeValue() {
-        if ((xCoord == 9999) || (yCoord == 9999)) {
-            return true;
-        }
+		xCoord = sc.nextDouble();
+		yCoord = sc.nextDouble();
 
-        return false;
-    }
+		if ((xCoord == 9999) || (yCoord == 9999)) {
+			missingNodes.add(i);
+		} else {
+			x.setAsDouble(xCoord, i, 0);
+			y.setAsDouble(yCoord, i, 0);
+			totalXCoord += xCoord;
+			totalYCoord += yCoord;
+		}
+	}
+
+	public boolean isValidNodeValue() {
+		if ((xCoord == 9999) || (yCoord == 9999)) {
+			return true;
+		}
+
+		return false;
+	}*/
 }
 
-
-//~ Formatted by Jindent --- http://www.jindent.com
+// ~ Formatted by Jindent --- http://www.jindent.com
