94f0cf6d3b2f8a153560058199ad0f8450339a48
Samuel C. Tyler <skamansam@gmail.com>
2009-12-17 15:59:54 +0000

    GUI: works importing CSV. Data Listing works!

--- GISpatialNet/src/us/jonesrychtar/gispatialnet/gui/GSNPanel/DataLister.java
+++ GISpatialNet/src/us/jonesrychtar/gispatialnet/gui/GSNPanel/DataLister.java
@@ -9,6 +9,17 @@ import java.util.logging.Logger;
 
 import javax.swing.JFrame;
 import javax.swing.JTree;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.MutableTreeNode;
+import javax.swing.tree.TreeModel;
+import javax.swing.tree.TreeNode;
+import javax.swing.tree.TreePath;
+
+import org.ujmp.core.Matrix;
+import org.ujmp.core.MatrixFactory;
 
 import us.jonesrychtar.gispatialnet.DataSet;
 import us.jonesrychtar.gispatialnet.GISpatialNet;
@@ -20,10 +31,38 @@ import us.jonesrychtar.gispatialnet.gui.helpers.CSVOptionsFrame;
  * @author sam
  *
  */
-public class DataLister extends JTree {
-	GISpatialNet gsn = new GISpatialNet();
+public class DataLister extends JTree implements TreeSelectionListener {
 
+	private static final long serialVersionUID = -337982300838439449L;
+
+	GISpatialNet gsn = new GISpatialNet();
+	static DefaultMutableTreeNode topNode = new DefaultMutableTreeNode("DataSets Loaded:");
+	static DefaultTreeModel tm = new DefaultTreeModel(topNode);
+
+	private class DSMTreeNode extends DefaultMutableTreeNode{
+		private int dsIdx = -1;
+		private String theTitle = "[No Label]";
+		private DataSet ds=null;
+		private Matrix m = MatrixFactory.emptyMatrix();
+		private boolean hasDS = false;
+		public DSMTreeNode(String t,Matrix mat){this.setMatrix(mat);this.setTitle(t);}
+		public DSMTreeNode(String t,DataSet d){this.setDataSet(d);this.setTitle(t);hasDS=true;}
+		public void setIndex(int dsIdx) {this.dsIdx = dsIdx;}
+		public int getIndex() {return dsIdx;}
+		public void setTitle(String theTitle) {this.theTitle = theTitle;}
+		public String getTitle() {return theTitle;}
+		public DataSet getDataSet() {return ds;}
+		public void setDataSet(DataSet ds) {this.ds = ds;}
+		public Matrix getMatrix() {return m;}
+		public void setMatrix(Matrix m) {this.m = m;}
+		public String toString(){return theTitle;}
+		public boolean hasDataSet(){return hasDS;}
+	}
+	
 	public DataLister(){
+		super(tm);
+		this.setRootVisible(false);
+		this.addTreeSelectionListener(this);
 	}
 
 	public GISpatialNet getGSN() {
@@ -35,35 +74,46 @@ public class DataLister extends JTree {
 	}
 	public void addCSV(String theFile){
 		Reader reader = new Reader();
-		String[] ops = getCSVOptions();
-        System.out.print("What is the field separator? ");
-
+		CSVOptionsFrame f = new CSVOptionsFrame();
         try {
-            Vector<DataSet> vds = Reader.loadTxt(theFile, new Integer(ops[0]), new Integer(ops[1]), new Integer(ops[2]), new Integer(ops[3]), ops[4].charAt(0));
+            Vector<DataSet> vds = Reader.loadTxt(theFile, f.getDataSetTypeAsInt(), f.getMatrixTypeAsInt(), f.getRowsAsInt(), f.getColumnsAsInt(), f.getSeparator());
 
             for (int i = 0; i < vds.size(); i++) {
-                if (ops[5].equals("1")) vds.elementAt(i).PolarToXY();
+                if (f.getIsPolar()) vds.elementAt(i).PolarToXY();
                 gsn.getDataSets().add(vds.elementAt(i));
             }
             reloadData();
         } catch (Exception ex) {
             Logger.getLogger(cli.class.getName()).log(Level.SEVERE, null, ex);
         }
+        reloadData();
 
 	}
 	
-	private void reloadData(){}
-	
-	public String[] getCSVOptions(){
-		//options are :
-		//{ ["Node data (nodes with attributes)", "Adjacency data", "Node data only", "Attribute data"],
-		// {Full, Upper, Lower},
-		//rows,cols,separator,isXYPolar?}
-		//JFrame csvOpts=new JFrame("CSV Import Options");
-		CSVOptionsFrame f = new CSVOptionsFrame();
-		String[] options={"0","0","25","2",",","0"};
-		return options;
+	private void reloadData(){
+		System.out.println(""+gsn.getDataSets().size()+" datasets are loaded!");
+		//TreeModel tm = this.getModel();
+		
+		//clear tree
+		while(topNode.getChildCount()!=0){
+			tm.removeNodeFromParent((MutableTreeNode) topNode.getChildAt(0));
+		}
+		
+		for(DataSet ds : gsn.getDataSets()){
+			DSMTreeNode next = new DSMTreeNode(ds.GetLoadedFiles().elementAt(0),ds);
+			//System.out.println("Inserting "+ds.GetLoadedFiles().elementAt(0));
+			next.add(new DSMTreeNode("X",ds.getX()) );
+			next.add(new DSMTreeNode("Y",ds.getY()) );
+			next.add(new DSMTreeNode("Adjacency",ds.getAdj()) );
+			next.add( new DSMTreeNode("Attributes",ds.getAttb()) );
+			topNode.add(next);
+			
+		}
+
+		tm.setRoot(topNode);
+		
 	}
+	
 	public void addExcel(String theFile) {
 		// TODO Auto-generated method stub
 		
@@ -82,5 +132,12 @@ public class DataLister extends JTree {
 	public void addShapefile(String theFile) {
 		// TODO Auto-generated method stub
 		
+	}
+
+	@Override
+	public void valueChanged(TreeSelectionEvent e) {
+		DSMTreeNode dt = (DSMTreeNode) this.getLastSelectedPathComponent();
+		if(!dt.hasDataSet())
+			System.out.print("Selected :\n"+dt.getMatrix());
 	}
 }
--- GISpatialNet/src/us/jonesrychtar/gispatialnet/gui/helpers/CSVOptionsFrame.java
+++ GISpatialNet/src/us/jonesrychtar/gispatialnet/gui/helpers/CSVOptionsFrame.java
@@ -3,75 +3,136 @@
  */
 package us.jonesrychtar.gispatialnet.gui.helpers;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JSeparator;
 import javax.swing.JSpinner;
 import javax.swing.SpinnerNumberModel;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 /**
  * @author sam
- *
+ * 
  */
-public class CSVOptionsFrame extends JFrame {
-	int dsType=0,matrixType=0,rows=0,cols=0;
-	char separator=',';
+public class CSVOptionsFrame extends JDialog implements ActionListener,
+		ChangeListener, KeyListener {
+	int dsType = 0, matrixType = 0, rows = 24, cols = 4;
+	char separator = ',';
+	boolean isPolar = false;
 	boolean hasCancelled = false;
-	
-	public CSVOptionsFrame(){
+	JComboBox dst = new JComboBox(new String[] {
+			"Node data (nodes with attributes)", "Adjacency data",
+			"Node data only", "Attribute data" });
+	JComboBox mt = new JComboBox(new String[] { "Full", "Upper", "Lower" });
+	JSpinner rowSpinner = new JSpinner(new SpinnerNumberModel(25, 0, 999999, 1));
+	JSpinner colSpinner = new JSpinner(new SpinnerNumberModel(4, 0, 999999, 1));
+	JComboBox coord = new JComboBox(new String[] { "X,Y",
+			"Polar (decimal and degrees)" });
+	JComboBox sep = new JComboBox(new String[] { "Comma",
+	"Tab","Space" });
+
+	public CSVOptionsFrame() {
 		this.setTitle("CSV Import Options");
 		this.setDefaultCloseOperation(HIDE_ON_CLOSE);
-		this.setBounds(100, 100, 400, 200);
+		this.setBounds(100, 100, 500, 350);
+
 		JPanel p = new JPanel();
-		p.setLayout(new BoxLayout(p,BoxLayout.Y_AXIS));
-		this.add(p);		
+		p.setLayout(new BoxLayout(p, BoxLayout.Y_AXIS));
+		this.add(p);
+
+		dst.addActionListener(this);
 		JLabel dstl = new JLabel("Data to read: ");
-		JComboBox dst = new JComboBox(new String[]{"Node data (nodes with attributes)", "Adjacency data", "Node data only", "Attribute data"});
-		//dst.setListData(new String[]{"Node data (nodes with attributes)", "Adjacency data", "Node data only", "Attribute data"});
 		JPanel r1 = new JPanel();
-		r1.add(dstl);r1.add(dst);
+		r1.add(dstl);
+		r1.add(dst);
 		p.add(r1);
+		mt.addActionListener(this);
 		JLabel mtl = new JLabel("Type of Matrix: ");
-		JComboBox mt = new JComboBox(new String[]{"Full","Upper","Lower"});
 		JPanel r2 = new JPanel();
-		r2.add(mtl);r2.add(mt);
+		r2.add(mtl);
+		r2.add(mt);
 		p.add(r2);
 
+		rowSpinner.addChangeListener(this);
 		JLabel rl = new JLabel("Rows to read: ");
-		JSpinner rowSpinner=new JSpinner(new SpinnerNumberModel(25,0,999999,1));
 		JPanel r3 = new JPanel();
-		r3.add(rl);r3.add(rowSpinner);
+		r3.add(rl);
+		r3.add(rowSpinner);
 		p.add(r3);
 
-		JLabel cl = new JLabel("Columns to read: ");
-		JSpinner colSpinner=new JSpinner(new SpinnerNumberModel(4,0,999999,1));
+		colSpinner.addChangeListener(this);
+		JLabel cl = new JLabel("Columns to read (split every X rows): ");
 		JPanel r4 = new JPanel();
-		r4.add(cl);r4.add(colSpinner);
+		r4.add(cl);
+		r4.add(colSpinner);
 		p.add(r4);
+
+		coord.addActionListener(this);
+		JLabel coordl = new JLabel("Coordinate System: ");
+		JPanel r6 = new JPanel();
+		r6.add(coordl);
+		r6.add(coord);
+		p.add(r6);
+
+		sep.addActionListener(this);
+		sep.addKeyListener(this);
+		sep.setEditable(true);
+		JLabel sepl = new JLabel("Field Delimiter: ");
+		JPanel r7 = new JPanel();
+		r7.add(sepl);
+		r7.add(sep);
+		p.add(r7);
 		
-		//TODO: add cancel/ok buttons
-		
+		p.add(new JSeparator());
+
+		// TODO: add cancel/ok buttons
+		JButton cancel = new JButton("Cancel");
+		cancel.addActionListener(this);
+		cancel.setActionCommand("cancel");
+		JButton ok = new JButton("Import");
+		ok.setActionCommand("ok");
+		ok.addActionListener(this);
+		JPanel r5 = new JPanel();
+		r5.add(cancel);
+		r5.add(ok);
+		p.add(r5);
+
+		//this.setAlwaysOnTop(true);
+		this.setModal(true);
 		this.setVisible(true);
 	}
-	
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		CSVOptionsFrame f = new CSVOptionsFrame();
-		System.out.println("DSType: "+f.getDataSetType());
-		System.out.println("MatrixType: "+f.getMatrixType());
-		System.out.println("Rows: "+f.getRows());
-		System.out.println("Columns: "+f.getColumns());
-		System.out.println("Separator: "+f.getSeperator());
+		System.out.println("\n=========\n");
+		f.printInfo();
 	}
-	
-	//["Node data (nodes with attributes)", "Adjacency data", "Node data only", "Attribute data"]
-	public String getDataSetType(){
-		switch(dsType){
+	public void printInfo(){
+		System.out.println("DSType: " + getDataSetType());
+		System.out.println("MatrixType: " + getMatrixType());
+		System.out.println("Rows: " + getRows());
+		System.out.println("Columns: " + getColumns());
+		System.out.println("Separator: '" + getSeparator()+"'");		
+	}
+	// ["Node data (nodes with attributes)", "Adjacency data", "Node data only",
+	// "Attribute data"]
+	public String getDataSetType() {
+		switch (dsType) {
 		case 0:
 			return "node_attb";
 		case 1:
@@ -84,11 +145,14 @@ public class CSVOptionsFrame extends JFrame {
 			return "node_attb";
 		}
 	}
-	public int getDataSetTypeAsInt(){return dsType;}
 
-	//{Full, Upper, Lower}
-	public String getMatrixType(){
-		switch(matrixType){
+	public int getDataSetTypeAsInt() {
+		return dsType;
+	}
+
+	// {Full, Upper, Lower}
+	public String getMatrixType() {
+		switch (matrixType) {
 		case 0:
 			return "full";
 		case 1:
@@ -99,14 +163,103 @@ public class CSVOptionsFrame extends JFrame {
 			return "full";
 		}
 	}
-	public int getMatrixTypeAsInt(){return matrixType;}
 
-	public String getRows(){return new Integer(rows).toString();}
-	public int getRowsAsInt(){return rows;}
+	public int getMatrixTypeAsInt() {
+		return matrixType;
+	}
 
-	public String getColumns(){return new Integer(cols).toString();}
-	public int getColumnsAsInt(){return cols;}
+	public String getRows() {
+		return new Integer(rows).toString();
+	}
 
-	public char getSeperator(){return separator;}
-	public String getColumnsAsString(){return new String(""+separator);}
+	public int getRowsAsInt() {
+		return rows;
+	}
+
+	public String getColumns() {
+		return new Integer(cols).toString();
+	}
+
+	public int getColumnsAsInt() {
+		return cols;
+	}
+
+	public char getSeparator() {
+		return separator;
+	}
+
+	public String getSeparatorAsString() {
+		return new String("" + separator);
+	}
+
+	public int getIsPolarAsInt() {
+		return this.isPolar ? 1 : 0;
+	}
+
+	public boolean getIsPolar() {
+		return this.isPolar;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getActionCommand().equals("cancel")) {
+			this.hasCancelled = true;
+			this.setVisible(false);
+		}
+		if (e.getActionCommand().equals("ok")) {
+			this.hasCancelled = false;
+			this.setVisible(false);
+		}
+
+		if(e.getSource().equals(dst))
+			this.dsType = dst.getSelectedIndex();
+		if(e.getSource().equals(mt))
+			this.matrixType = mt.getSelectedIndex();
+		if(e.getSource().equals(coord))
+			this.isPolar = coord.getSelectedIndex()==0?false:true;
+		if(e.getSource().equals(sep)){
+			String sepVal = (String)(sep.getEditor().getItem());
+			if(sepVal.equals("Tab"))
+				this.separator = '\u0009';
+			else if(sepVal.equals("Space"))
+				this.separator = ' ';
+			else if(sepVal.equals("Comma"))
+				this.separator = ',';
+			else
+				this.separator = sepVal.charAt(0);
+		}
+		printInfo();
+	}
+
+	@Override
+	public void stateChanged(ChangeEvent e) {
+		if(e.getSource().equals(rowSpinner))
+			this.rows = new Integer(rowSpinner.getValue().toString());
+		if(e.getSource().equals(colSpinner))
+			this.cols = new Integer(colSpinner.getValue().toString());
+		printInfo();
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if(e.getSource().equals(sep)){
+			String sepVal = (String)(sep.getEditor().getItem());
+			if(sepVal.equals("Tab"))
+				this.separator = '\u0009';
+			else if(sepVal.equals("Space"))
+				this.separator = ' ';
+			else if(sepVal.equals("Comma"))
+				this.separator = ',';
+			else
+				this.separator = sepVal.charAt(0);
+		}
+		printInfo();
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {}
+
+	@Override
+	public void keyTyped(KeyEvent e) {}
 }
