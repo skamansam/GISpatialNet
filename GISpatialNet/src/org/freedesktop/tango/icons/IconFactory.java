4bcab972f1c135a4bf880b815a114319302e83f4
Samuel C. Tyler <skamansam@gmail.com>
2010-07-29 19:33:33 +0000

    * Finished Tango IconFactory class.    -- Added convenience methods for getting default icons, like getErrorIcon(), for dialogs/menus/etc.    -- Added method for getting system desktop icons based on extension, with "_MIME_" parameter switch.         - If you call getIcon("_MIME_","xml,cvs,txt,[fallback category]/[fallback icon name]",22), you will get the first non-generic  icon for the filetypes in the list         - uses fallback icon if no system icon is set. * Added JDIC library for Desktop Connectivity. * Added the rest of my "Save All" icons * Updated menus to take advantage of new class.

--- GISpatialNet/src/org/freedesktop/tango/icons/IconFactory.java
+++ GISpatialNet/src/org/freedesktop/tango/icons/IconFactory.java
@@ -3,11 +3,21 @@
  */
 package org.freedesktop.tango.icons;
 
+import java.awt.Graphics2D;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.fop.svg.GraphicsConfiguration;
+import org.jdesktop.jdic.filetypes.AssociationService;
+import org.jdesktop.jdic.filetypes.Association;
+import javax.swing.filechooser.FileSystemView; 
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
 
 import us.jonesrychtar.gispatialnet.gui.GUIutil;
@@ -19,59 +29,66 @@ import us.jonesrychtar.gispatialnet.gui.GUIutil;
 public class IconFactory {
 	
 	public static ImageIcon getActionSmallIcon(String iconName){return getIcon("actions",iconName,16);}
-	public static ImageIcon getActionLargeIcon(String iconName){return getIcon("actions",iconName,16);}
-	public static ImageIcon getActionIcon(String iconName){return getIcon("actions",iconName,16);}
+	public static ImageIcon getActionLargeIcon(String iconName){return getIcon("actions",iconName,32);}
+	public static ImageIcon getActionIcon(String iconName){return getIcon("actions",iconName,22);}
 
 	public static ImageIcon getAnimationSmallIcon(String iconName){return getIcon("animations",iconName,16);}
-	public static ImageIcon getAnimationLargeIcon(String iconName){return getIcon("animations",iconName,16);}
-	public static ImageIcon getAnimationIcon(String iconName){return getIcon("animations",iconName,16);}
+	public static ImageIcon getAnimationLargeIcon(String iconName){return getIcon("animations",iconName,32);}
+	public static ImageIcon getAnimationIcon(String iconName){return getIcon("animations",iconName,22);}
 	
 	public static ImageIcon getAppSmallIcon(String iconName){return getIcon("apps",iconName,16);}
-	public static ImageIcon getAppLargeIcon(String iconName){return getIcon("apps",iconName,16);}
-	public static ImageIcon getAppIcon(String iconName){return getIcon("apps",iconName,16);}
+	public static ImageIcon getAppLargeIcon(String iconName){return getIcon("apps",iconName,32);}
+	public static ImageIcon getAppIcon(String iconName){return getIcon("apps",iconName,22);}
 
 	public static ImageIcon getCategorySmallIcon(String iconName){return getIcon("categories",iconName,16);}
-	public static ImageIcon getCategoryLargeIcon(String iconName){return getIcon("categories",iconName,16);}
-	public static ImageIcon getCategoryIcon(String iconName){return getIcon("categories",iconName,16);}
+	public static ImageIcon getCategoryLargeIcon(String iconName){return getIcon("categories",iconName,32);}
+	public static ImageIcon getCategoryIcon(String iconName){return getIcon("categories",iconName,22);}
 
 	public static ImageIcon getDeviceSmallIcon(String iconName){return getIcon("devices",iconName,16);}
 	public static ImageIcon getDeviceLargeIcon(String iconName){return getIcon("devices",iconName,16);}
 	public static ImageIcon getDeviceIcon(String iconName){return getIcon("devices",iconName,16);}
 
 	public static ImageIcon getEmblemSmallIcon(String iconName){return getIcon("emblems",iconName,16);}
-	public static ImageIcon getEmblemLargeIcon(String iconName){return getIcon("emblems",iconName,16);}
-	public static ImageIcon getEmblemIcon(String iconName){return getIcon("emblems",iconName,16);}
+	public static ImageIcon getEmblemLargeIcon(String iconName){return getIcon("emblems",iconName,32);}
+	public static ImageIcon getEmblemIcon(String iconName){return getIcon("emblems",iconName,22);}
 
 	public static ImageIcon getEmoteSmallIcon(String iconName){return getIcon("emotes",iconName,16);}
-	public static ImageIcon getEmoteLargeIcon(String iconName){return getIcon("emotes",iconName,16);}
-	public static ImageIcon getEmoteIcon(String iconName){return getIcon("emotes",iconName,16);}
+	public static ImageIcon getEmoteLargeIcon(String iconName){return getIcon("emotes",iconName,32);}
+	public static ImageIcon getEmoteIcon(String iconName){return getIcon("emotes",iconName,22);}
 
 	public static ImageIcon getMimeTypeSmallIcon(String iconName){return getIcon("mimetypes",iconName,16);}
-	public static ImageIcon getMimeTypeLargeIcon(String iconName){return getIcon("mimetypes",iconName,16);}
-	public static ImageIcon getMimeTypeIcon(String iconName){return getIcon("mimetypes",iconName,16);}
+	public static ImageIcon getMimeTypeLargeIcon(String iconName){return getIcon("mimetypes",iconName,32);}
+	public static ImageIcon getMimeTypeIcon(String iconName){return getIcon("mimetypes",iconName,22);}
 
 	public static ImageIcon getPlaceSmallIcon(String iconName){return getIcon("places",iconName,16);}
-	public static ImageIcon getPlaceLargeIcon(String iconName){return getIcon("places",iconName,16);}
-	public static ImageIcon getPlaceIcon(String iconName){return getIcon("places",iconName,16);}
+	public static ImageIcon getPlaceLargeIcon(String iconName){return getIcon("places",iconName,32);}
+	public static ImageIcon getPlaceIcon(String iconName){return getIcon("places",iconName,22);}
 
 	public static ImageIcon getStatusSmallIcon(String iconName){return getIcon("status",iconName,16);}
-	public static ImageIcon getStatusLargeIcon(String iconName){return getIcon("status",iconName,16);}
-	public static ImageIcon getStatusIcon(String iconName){return getIcon("status",iconName,16);}
+	public static ImageIcon getStatusLargeIcon(String iconName){return getIcon("status",iconName,32);}
+	public static ImageIcon getStatusIcon(String iconName){return getIcon("status",iconName,22);}
+	
+	public static ImageIcon getInfoSmallIcon(){return getIcon("status","dialog-information",16);}
+	public static ImageIcon getInfoIcon(){return getIcon("status","dialog-information",22);}
+	public static ImageIcon getInfoLargeIcon(){return getIcon("status","dialog-information",32);}
+
+	public static ImageIcon getErrorSmallIcon(){return getIcon("status","dialog-error",16);}
+	public static ImageIcon getErrorIcon(){return getIcon("status","dialog-error",22);}
+	public static ImageIcon getErrorLargeIcon(){return getIcon("status","dialog-error",32);}
+	
+	public static ImageIcon getWarningSmallIcon(){return getIcon("status","dialog-warning",16);}
+	public static ImageIcon getWarningIcon(){return getIcon("status","dialog-warning",22);}
+	public static ImageIcon getWarningLargeIcon(){return getIcon("status","dialog-warning",32);}
 	
 	
-	/**Retrieves icons from the jlfgr-1_0.jar file. There are several convenience methods for this function.
-	 * They are in the form of getXXXIcon(iconName), where XXX is the category name (folder name)
-	 * and whether we want small (16x16) icons. For example, if we want to get the file 
-	 * /toolbarButtonGraphics/general/SaveAs16.gif,
-	 * we would use getGeneralSmallIcon("SaveAs"). If we want the 24x24 size version, we would use 
-	 * getGeneralIcon("SaveAs"). You can find these icons for download, as well as more information, from 
-	 * http://java.sun.com/developer/techDocs/hi/repository/
-	 * @param category the category name. corresponds to the folder under /toolbarButtonGraphics/
-	 * @param iconName the name of the icon. corresponds to the file under /toolbarButtonGraphics/[category]/iconName[16|24].gif
-	 * @param size whether we want 16x16 (size=16) or 24x24 (size=24) icons
+	/** This method returns the ImageIcon which represents one of the Tango Icon files. 
+	 * @param category the category name. corresponds to the folder under org.freedesktop.tango.icons/[size]x[size]/
+	 * @param iconName the name of the icon. corresponds to the file under org.freedesktop.tango.icons/[size]x[size]/[category].gif
+	 * @param size whether we want 16x16 (size=16) or 22x22 (size=24) or 32x32 (size=32) icons. Nearest values will be rounded.
 	 * @return an ImageIcon which represents the given parameters
 	 */
-	public static ImageIcon getIcon(String category, String iconName, int size){ 
+	public static ImageIcon getIcon(String category, String iconName, int size){
+		if(category.equals("_MIME_"))return getFileTypeIcon(iconName,size);
 		if(size==24)size=22;
 		URL url=GUIutil.class.getResource("/org/freedesktop/tango/icons/"+size+'x'+size+"/"+category+"/"+iconName+".png");
 		if(url != null){
@@ -82,6 +99,65 @@ public class IconFactory {
 		}
 	}
 
+	/**
+	 * This method retrieves the icon for a given filetype. It will return on the first unique match (it fails if the icon is the default "unknown" icon). If there is no unique icon, the last value in the suffix list will be used to call getIcon().
+	 * @param suffix A string list of suffixes for the file icon you want. The last value is category/iconName for the fallback icon. (ex: "txt,html,mimetypes/text-html")
+	 * @param size the icon size. Only used for fallback icon.
+	 * @return the ImageIcon
+	 */
+	public static ImageIcon getFileTypeIcon(String suffix,int size){
+		String[] exts=suffix.split(",");
+		//System.out.println("Looking for extensions for "+exts.length);
+		ImageIcon ico = new ImageIcon();
+		ImageIcon dummy = new ImageIcon();
+		String[] fallback=exts[exts.length-1].split("/");
+		
+		//create dummy icon so we can compare non-existant icons with custom icons
+		try{
+			File f = File.createTempFile("icon", ".unknown");
+			dummy = icon2ImageIcon(FileSystemView.getFileSystemView().getSystemIcon(f));
+			f.delete();
+		}catch (IOException ioe){
+			System.out.println("Cannot create dummy image.");
+		}
+
+		for(int i=0;i<exts.length-1;i++){
+			System.out.print("MIME icon for "+exts[i]+"\n");
+			try{
+				File f = File.createTempFile("icon", "." + exts[i]);
+				ico = icon2ImageIcon(FileSystemView.getFileSystemView().getSystemIcon(f));
+				f.delete();
+				if(ico.getIconHeight()==0 || ico.getIconWidth()==0||ico.getImage().equals(dummy.getImage()))continue;
+			}catch (IOException ioe){
+				continue;
+			}
+			break;
+		}
+		//use fallback icon
+		if(ico.getImage().equals(dummy.getImage())){
+			ico=getIcon(fallback[0],fallback[1],size);
+		}
+
+		return ico;
+	}
+
+	public static ImageIcon icon2ImageIcon(Icon icon) {
+	    if (icon instanceof ImageIcon) {
+	        return ((ImageIcon)icon);
+	    } else {
+	        int w = icon.getIconWidth();
+	        int h = icon.getIconHeight();
+	        GraphicsEnvironment ge =
+	          GraphicsEnvironment.getLocalGraphicsEnvironment();
+	        GraphicsDevice gd = ge.getDefaultScreenDevice();
+	        GraphicsConfiguration gc = (GraphicsConfiguration) gd.getDefaultConfiguration();
+	        BufferedImage image = gc.createCompatibleImage(w, h);
+	        Graphics2D g = image.createGraphics();
+	        icon.paintIcon(null, g, 0, 0);
+	        g.dispose();
+	        return new ImageIcon(image);
+	    }
+	} 
 	/**
 	 * @param args
 	 */
--- GISpatialNet/src/us/jonesrychtar/gispatialnet/gui/GSNMenuBar.java
+++ GISpatialNet/src/us/jonesrychtar/gispatialnet/gui/GSNMenuBar.java
@@ -48,53 +48,53 @@ public class GSNMenuBar extends JMenuBar implements MouseListener{
 		//file menu
 		f_open = createMenuItem(fileMenu,"Open...",KeyEvent.VK_O,"Open a file.","actions","document-open","open");
 		f_save = createMenuItem(fileMenu,"Save...",KeyEvent.VK_S,"Save currently selected data set.","actions","document-save","save");
-		//f_save_as = createMenuItem(fileMenu,"Save As...",KeyEvent.VK_SHIFT+KeyEvent.VK_S,"Save as a different file.","general","document-save-as","save_as");
+		f_save_as.setIcon(GUIutil.getTangoIcon("actions","document-save-as",16));
 		fileMenu.add(f_save_as);
 		f_save_all = createMenuItem(fileMenu,"Save All",KeyEvent.VK_SHIFT+KeyEvent.VK_A,"Save all data sets.","actions","document-save-all","save_all");
 		fileMenu.addSeparator();
-		f_quit = createMenuItem(fileMenu,"Quit",KeyEvent.VK_Q,"Exit Program","general","Stop","exit");
+		f_quit = createMenuItem(fileMenu,"Quit",KeyEvent.VK_Q,"Exit Program","actions","system-log-out","exit");
 
-		sa_csv = createMenuItem(f_save_as,"CSV",KeyEvent.VK_M,"Save as a CSV file.","general","SaveAs","save_as_csv");
-		sa_pajek = createMenuItem(f_save_as,"Pajek",KeyEvent.VK_N,"Save as a Pajek file..","general","SaveAs","save_as_pajek");
-		sa_shapefile = createMenuItem(f_save_as,"Shapefile",KeyEvent.VK_Z,"Save as a different file.","general","SaveAs","save_as_shapefile");
-		sa_ucinet = createMenuItem(f_save_as,"DL / UCINet",KeyEvent.VK_D,"Save as a different file.","general","SaveAs","save_as_ucinet");
-		sa_excel = createMenuItem(f_save_as,"Excel",KeyEvent.VK_C,"Save as a different file.","general","SaveAs","save_as_excel");
+		sa_csv = createMenuItem(f_save_as,"CSV",KeyEvent.VK_M,"Save as a CSV file.","mimetypes","text-x-generic","save_as_csv");
+		sa_pajek = createMenuItem(f_save_as,"Pajek",KeyEvent.VK_N,"Save as a Pajek file..","mimetypes","font-x-generic","save_as_pajek");
+		sa_shapefile = createMenuItem(f_save_as,"Shapefile",KeyEvent.VK_Z,"Save as a different file.","mimetypes","x-office-drawing","save_as_shapefile");
+		sa_ucinet = createMenuItem(f_save_as,"DL / UCINet",KeyEvent.VK_D,"Save as a different file.","mimetypes","text-x-script","save_as_ucinet");
+		sa_excel = createMenuItem(f_save_as,"Excel",KeyEvent.VK_C,"Save as a different file.","mimetypes","x-office-spreadsheet","save_as_excel");
 		
 		//transform menu
-		t_flip = createMenuItem(transformMenu,"Flip",KeyEvent.VK_T,"Flip","general","Redo","transform_flip");
-		t_rotate = createMenuItem(transformMenu,"Rotate",KeyEvent.VK_R,"Rotate","general","Refresh","transform_rotate");
-		t_resize = createMenuItem(transformMenu,"Resize",KeyEvent.VK_Q,"Resize","general","ZoomIn","transform_resize");
-		t_match_best = createMenuItem(transformMenu,"Match Best",KeyEvent.VK_Q,"Fuzzy logic for finding duplicates","general","Preferences","transform_match_best");
-		t_find_duplicate = createMenuItem(transformMenu,"Find Similar",KeyEvent.VK_Q,"Find duplicate items to the selected item","general","Preferences","find_similar");
+		t_flip = createMenuItem(transformMenu,"Flip",KeyEvent.VK_T,"Flip","actions","go-last","transform_flip");
+		t_rotate = createMenuItem(transformMenu,"Rotate",KeyEvent.VK_R,"Rotate","actions","view-refresh","transform_rotate");
+		t_resize = createMenuItem(transformMenu,"Resize",KeyEvent.VK_Q,"Resize","actions","edit-find","transform_resize");
+		t_match_best = createMenuItem(transformMenu,"Match Best",KeyEvent.VK_Q,"Fuzzy logic for finding duplicates","actions","edit-find-replace","transform_match_best");
+		t_find_duplicate = createMenuItem(transformMenu,"Find Similar",KeyEvent.VK_Q,"Find duplicate items to the selected item","actions","edit-clear","find_similar");
 		
 
 		//algorithm menu
-		a_qap = createMenuItem(algorithmMenu,"QAP (Mantel's Test)",KeyEvent.VK_P,"Compare two matrices using Mantel's test.","general","History","algorithm_qap");
-		a_snb = createMenuItem(algorithmMenu,"Spatial Network Bias",KeyEvent.VK_E,"Run a spatial network bias on the selected data.","general","History","algorithm_snb");
-		a_borders = createMenuItem(algorithmMenu,"Borders",KeyEvent.VK_B,"Borders","general","History","algorithm_borders");
-		a_hilite_edges = createMenuItem(algorithmMenu,"Highlight Edges",KeyEvent.VK_H,"Highlight Edges","general","History","algorithm_hilite_edges");
+		a_qap = createMenuItem(algorithmMenu,"QAP (Mantel's Test)",KeyEvent.VK_P,"Compare two matrices using Mantel's test.","actions","edit-copy","algorithm_qap");
+		a_snb = createMenuItem(algorithmMenu,"Spatial Network Bias",KeyEvent.VK_E,"Run a spatial network bias on the selected data.","categories","applications-internet","algorithm_snb");
+		a_borders = createMenuItem(algorithmMenu,"Borders",KeyEvent.VK_B,"Borders","actions","view-fullscreen","algorithm_borders");
+		a_hilite_edges = createMenuItem(algorithmMenu,"Highlight Edges",KeyEvent.VK_H,"Highlight Edges","mimetypes","x-office-document-template","algorithm_hilite_edges");
 
 		//dataset menu
-		d_find = createMenuItem(datasetMenu,"Clear",KeyEvent.VK_MINUS,"Clear all data","general","Delete","data_clear");
-		d_clear = createMenuItem(datasetMenu,"Find...",KeyEvent.VK_F,"Find content within data","general","Search","data_find");
-		d_merge = createMenuItem(datasetMenu,"Merge",KeyEvent.VK_M,"Merge this data set with another.","general","Import","data_merge");
-		d_seperate = createMenuItem(datasetMenu,"Seperate",KeyEvent.VK_DIVIDE,"Seperate data set into multiple sets.","table","RowDelete","data_separate");
-		d_add = createMenuItem(datasetMenu,"Add...",KeyEvent.VK_ADD,"Add data to the existing data.","general","Add","data_add");
-		d_add_ego = createMenuItem(datasetMenu,"Add Ego...",KeyEvent.VK_SHIFT+KeyEvent.VK_ADD,"Add an ego file to the current data set.","general","Add","data_add_ego");
+		d_find = createMenuItem(datasetMenu,"Clear",KeyEvent.VK_MINUS,"Clear all data","actions","edit-clear","data_clear");
+		d_clear = createMenuItem(datasetMenu,"Find...",KeyEvent.VK_F,"Find content within data","actions","edit-find","data_find");
+		d_merge = createMenuItem(datasetMenu,"Merge",KeyEvent.VK_M,"Merge this data set with another.","actions","edit-copy","data_merge");
+		d_seperate = createMenuItem(datasetMenu,"Seperate",KeyEvent.VK_DIVIDE,"Seperate data set into multiple sets.","actions","edit-copy","data_separate");
+		d_add = createMenuItem(datasetMenu,"Add...",KeyEvent.VK_ADD,"Add data to the existing data.","actions","list-add","data_add");
+		d_add_ego = createMenuItem(datasetMenu,"Add Ego...",KeyEvent.VK_SHIFT+KeyEvent.VK_ADD,"Add an ego file to the current data set.","actions","list-add","data_add_ego");
 
 		//view with menu
-		v_pajek = createMenuItem(viewMenu,"Pajek",KeyEvent.VK_F,"Open current data set in Pajek.","development","WebComponentAdd","view_pajek");
-		v_egonet = createMenuItem(viewMenu,"EgoNet",KeyEvent.VK_ADD,"Open current data set in EgoNet.","development","WebComponentAdd","view_egonet");
-		v_ucinet = createMenuItem(viewMenu,"UCINet",KeyEvent.VK_SHIFT+KeyEvent.VK_ADD,"Open current data set in ucinet.","development","WebComponentAdd","view_ucinet");
-		v_geotools = createMenuItem(viewMenu,"GeoTools",KeyEvent.VK_ADD,"Open current data set in GeoTools.","development","WebComponentAdd","view_geotools");
-		v_arcgis = createMenuItem(viewMenu,"ArcGIS",KeyEvent.VK_SHIFT+KeyEvent.VK_ADD,"Open current data set in ArcGIS.","development","WebComponentAdd","view_arcgis");
-		v_g_earth = createMenuItem(viewMenu,"Google Earth",KeyEvent.VK_M,"Open current data set in Google Earth.","development","WebComponentAdd","view_g_earth");
-		v_g_maps = createMenuItem(viewMenu,"Google Maps",KeyEvent.VK_DIVIDE,"Open current data set in Google Maps.","development","WebComponentAdd","view_g_maps");
+		v_pajek = createMenuItem(viewMenu,"Pajek",KeyEvent.VK_F,"Open current data set in Pajek.","_MIME_","net,mat,mimetypes/font-x-generic","view_pajek");
+		v_egonet = createMenuItem(viewMenu,"EgoNet",KeyEvent.VK_ADD,"Open current data set in EgoNet.","_MIME_","dat,mimetypes/font-x-generic","view_egonet");
+		v_ucinet = createMenuItem(viewMenu,"UCINet",KeyEvent.VK_SHIFT+KeyEvent.VK_ADD,"Open current data set in ucinet.","_MIME_","dat,txt,mimetypes/font-x-generic","view_ucinet");
+		v_geotools = createMenuItem(viewMenu,"uDig",KeyEvent.VK_ADD,"Open current data set in GeoTools.","_MIME_","csv,mimetypes/font-x-generic","view_uDig");
+		v_arcgis = createMenuItem(viewMenu,"ArcGIS",KeyEvent.VK_SHIFT+KeyEvent.VK_ADD,"Open current data set in ArcGIS.","_MIME_","shp,mimetypes/font-x-generic","view_arcgis");
+		v_g_earth = createMenuItem(viewMenu,"Google Earth",KeyEvent.VK_M,"Open current data set in Google Earth.","_MIME_","kml,kmz,xml,mimetypes/font-x-generic","view_g_earth");
+		v_g_maps = createMenuItem(viewMenu,"Google Maps",KeyEvent.VK_DIVIDE,"Open current data set in Google Maps.","_MIME_","html,htm,xml,mimetypes/font-x-generic","view_g_maps");
 		
 		
 		//help menu
-		h_about = createMenuItem(helpMenu,"About...",KeyEvent.VK_F12,"Information about GISpatialnet","general","TipOfTheDay","show_about");
-		h_help = createMenuItem(helpMenu,"Help...",KeyEvent.VK_F1,"Display online help","general","About","show_help");
+		h_about = createMenuItem(helpMenu,"About...",KeyEvent.VK_F12,"Information about GISpatialnet","status","dialog-information","show_about");
+		h_help = createMenuItem(helpMenu,"Help...",KeyEvent.VK_F1,"Display online help","apps","help-browser","show_help");
 		
 		//add top-level items
 		this.add(fileMenu);
